38f55182f48fd85e84ba5c3d9d8065d09d0c5a06
Emanuel <andreuemanuel@gmail.com>
2018-10-31 23:40:09 -0300

    agregamos código de inserción de artículos

--- src/pages/ventas/ventas.ts
+++ src/pages/ventas/ventas.ts
@@ -16,6 +16,13 @@ import { PopoverComponent } from '../../components/popover/popover';
 })
 export class VentasPage {
 
+  public comprobante = {
+    numero: 0,
+    puntoDeVenta: 1,
+    fechaEmision: new Date(),
+    total: 60
+  };
+
   public articulos = [
     {nombre: 'Milanesa', precio: 25.00, url: 'https://http2.mlstatic.com/milanesas-de-pollo-D_NQ_NP_979460-MLA27152434356_042018-F.jpg'},
     {nombre: 'Bife', precio: 25.00, url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRwSqvONBEvMWiKqOHFSItGtoG51WsyyLzKMl8D4Egeimj9C9si'},
@@ -45,67 +52,7 @@ export class VentasPage {
       cantidad: 1,
       importe: 35.00,
       total: 35.00
-    },
-    {
-      nombre: 'Milanesa',
-      cantidad: 2,
-      importe: 12.50,
-      total: 25.00
-    },
-    {
-      nombre: 'Coca Cola',
-      cantidad: 1,
-      importe: 35.00,
-      total: 35.00
-    },
-    {
-      nombre: 'Milanesa',
-      cantidad: 2,
-      importe: 12.50,
-      total: 25.00
-    },
-    {
-      nombre: 'Coca Cola',
-      cantidad: 1,
-      importe: 35.00,
-      total: 35.00
-    },
-    {
-      nombre: 'Milanesa',
-      cantidad: 2,
-      importe: 12.50,
-      total: 25.00
-    },
-    {
-      nombre: 'Coca Cola',
-      cantidad: 1,
-      importe: 35.00,
-      total: 35.00
-    },
-    {
-      nombre: 'Milanesa',
-      cantidad: 2,
-      importe: 12.50,
-      total: 25.00
-    },
-    {
-      nombre: 'Coca Cola',
-      cantidad: 1,
-      importe: 35.00,
-      total: 35.00
-    },
-    {
-      nombre: 'Milanesa',
-      cantidad: 2,
-      importe: 12.50,
-      total: 25.00
-    },
-    {
-      nombre: 'Coca Cola',
-      cantidad: 1,
-      importe: 35.00,
-      total: 35.00
-    },
+    }
   ];
 
   segmentosArticulos: string = "todos";
@@ -118,6 +65,25 @@ export class VentasPage {
     popover.present({
       ev: myEvent
     });
+  }
+
+  agregarArticulo(articulo) {
+    this.items.push({
+        nombre: articulo.nombre,
+        cantidad: 1,
+        importe: articulo.precio,
+        total: articulo.precio
+    });
+
+    this.calcularTotales();
+  }
+
+  calcularTotales(){
+    let tmpTotal: number = 0;
+    for(let i=0; i<this.items.length; i++){
+      tmpTotal += this.items[i].total;
+    }
+    this.comprobante.total = tmpTotal;
   }
 
 }
